f4cad5ebcb790c08cc3cd528294ec0e218ad2704
AlvesDavid01 <david.15addams@gmail.com>
2024-03-07 14:21:21 -0300

    Criando um arquivo main
    Criando arquivo main para deixar guardado a atividade avaliativa que eu fiz hoje.

---
+++ main.cs
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+class HelloWorld {
+    
+    for(;;) 
+    
+    int personagem;
+    int numero;
+    float redes sociais;
+    double requesito1;
+    
+  static void Main() {
+  {
+    Console.WriteLine("O nome do personagem: ");
+    Console.WriteLine("Nome do Usuário: ");
+    Console.WriteLine("Números de seguidores: ");
+
+    string numerosString = Console.ReadLine().Split('');
+    string redessociasString = Console.ReadLine(); 
+    int numero = int.Parse(numerosString[0]);
+    double media = (numero1)/ 3.0;
+    Console.WriteLine($"A média dos números é: {media}");
+    int numero = 0; 
+  }  
+     for(int i = 0; i < int.MaxValue; i ++ )
+     {
+        numero ++;
+     }
+     switch(requesito1)
+       
+        case 1:
+          
+            Console.WriteLine("+");
+            break;
+            
+        case 2:
+            Console.WriteLine("-");
+            break;
+            
+        case 3:
+        Console.WriteLine("/");
+        break;
+     {
+         Console.WriteLine("");
+     }
+  }
+}
